8e0dcd665783849a2e5d77010569bb8e22edfaae
moazOS <moaz.zekry3@gmail.com>
2023-05-15 21:42:56 +0300

    5las hant haaaaaant

--- omnia/Source.cpp
+++ omnia/Source.cpp
@@ -28,20 +28,18 @@ float TOTALUSERS=0;
 
 
 
-
+bool ID_validation(int id);
 void Register(string user_name, int user_id, string user_pass, string user_gender, int user_age, string user_govern, string user_vaccinted, string num_of_dose);
 void Login(string, string);
-void Password_validation(string);
-void ID_validation(string);
+bool Password_ID_validation(int id, string password);
 void DisplayMyRecord();
-void edit_user_data();
+void edit_user_data(string choose);
 void Delete_MyACC();
-void vaccinationState_stat();
 void load();
 void save();
 void accepting_vaccinated(int number_of_user);
 void get_vaccinted_user();
-
+void mainMenu();
 //--------------------- admin ------------------------------
 
 void display_perc_of_gender(char kind);
@@ -49,7 +47,7 @@ void display_perc_of_doses(int dose_num);
 void display_record(int id);
 void display_records_filterd(string num_doses , int age);
 void delete_record(int id);
-
+void adminMenu();
 
 //----------------------------------------------------------
 
@@ -60,196 +58,16 @@ void delete_record(int id);
 
 int main() {
  
-    Register("moaz", 1, "27007447", "male", 19, "cairo", "yes", "1");
-    Register("maryam", 4, "142003", "girl", 20, "cairo", "yes", "3");
-    Register("omnia", 6, "27007576", "girl", 20, "cairo", "yes", "2");
-    Register("nada", 2, "27007511", "girl", 18, "cairo", "yes", "3");
-    Register("saleh", 5, "27007555", "male", 19, "sudan", "yes", "3");
-    Register("youssef", 9, "27007787", "male", 20, "cairo", "no", "0");
-    Register("malak", 8, "27007333", "girl", 19, "cairo", "no", "0");
-    Register("kiro", 7, "27007444", "male", 21, "cairo", "yes", "2");
-    Register("menna", 3, "27007123", "girl", 21, "cairo", "no", "0");
-    display_perc_of_gender('m');
-    display_perc_of_gender('f');
-    cout << "------------------------\n";
-
-     display_perc_of_doses(0);
-     display_perc_of_doses(1);
-     display_perc_of_doses(2);
-     cout << "------------------------\n";
-
-     display_record(4);
-     cout << "------------------------\n";
-     display_records_filterd("2",20);
-     cout << "------------------------\n";
-     delete_record(5);
-     display_record(5);
-
-    /*cout << users[0].id << endl;*/
     
-    cout <<TOTALUSERS << endl;
+    Users admin;
+    users_vec.push_back(admin);
+    users.insert(make_pair(admin.id, admin));
+    usersID.insert(admin.id);
+    mainMenu();
 
     
     
-   /* int choice;
-    int option;
-    int your_choice;
-    string x;
-    string id;
-    string password;
-    string username;
-    cout << endl;
-    cout << "               ***********************************************           " << endl;
-    cout << "                    WELCOME TO VACCINE TRACKING SYSTEM                    " << endl;
-    cout << "               ***********************************************           " << endl;
-    cout << endl;
-    cout << "\t\t 1.User        \t\t\t2.Admin \n\t\t\t\t\     3. Exit" << endl;
-    cout << endl;
-    cout << " Please enter your choice:-" << endl;
-    cin >> choice;
-    switch (choice)
-    {
-    case 1:
-    {
-        do {
-            cout << "Please enter your choice: \n 1.Don't have an account ? (SignUp)\n  2.Display your record\n  3.Edit your record\n   4.Delete your record" << endl;
-            cin >> option;
-            switch (option)
-            {
-            case 1:
-            {
-               
 
-
-                break;
-            }
-            case 2:
-            {
-                cout << "Enter your id \n";
-                cin >> id;
-                cout << "Enter your password \n";
-                cin >> password;
-                string user_id ;
-                cout << "                    WELCOME USER                 " << endl;
-                cout << "Enter your choice: (1-Display your record 2-Edit your record  3-Delete your record)" << endl;
-                cin >> your_choice;
-                switch (your_choice)
-                {
-                case 1:
-                {
-                    
-                    break;
-                }
-                case 2:
-                {
-                    
-                    break;
-                }
-                case 3:
-                {
-                    
-                    break;
-
-                }
-                }
-                break;
-            }
-            }
-            cout << "Do you want to continue ? \n only enter y  " << endl;
-            cin >> x;
-
-        } while (x == "y" || x == "Y");
-
-        break;
-    }
-
-    case 2:
-    {
-
-        do {
-
-            cout << "----------------LOGIN AS AN ADMIN------------------- " << endl;
-
-            cout << "enter your username \n";
-            cin >> username;
-            cout << "enter your password \n";
-            cin >> password;
-           
-            cout << "Enter your choice:\n 1-Display one user only \n 2-Display all users \n  3-Delete one user only \n  4-Delete all users \n 5-View users filtered by doses \n 6-View records ordered by their age \n 7-View the waiting list \n 8- View % of first dose people \n 9-View % of both dose people \n 10-View % of males and females in the sytsem" << endl;
-            cin >> your_choice;
-            switch (your_choice)
-            {
-            case 1:
-            {
-
-                cout << "Enter User's id" << endl;
-                cin >> id;
-               
-                break;
-            }
-            case 2:
-            {
-                
-                break;
-            }
-            case 3:
-            {
-                cout << "Enter User's id" << endl;
-                cin >> id;
-                
-                break;
-
-            }
-            case 4: {
-                
-                break;
-            }
-            case 5: {
-
-                break;
-            }
-            case 6: {
-
-                break;
-            }
-            case 7: {
-
-                break;
-            }
-            case 8: {
-
-                break;
-            }
-            case 9: {
-
-                break;
-            }
-            case 10: {
-
-                break;
-            }
-            }
-            cout << "Do you want to continue ?" << endl;
-            cin >> x;
-        } while (x == "y" || x == "Y");
-        break;
-
-    }
-
-
-    case 3:
-    {
-        cout << "               ***********************************************           " << endl;
-        cout << "                      THANKS FOR USING OUR APPLICATION                  " << endl;
-        cout << "                              HAVE A NICE DAY !             " << endl;
-        cout << "               ***********************************************           " << endl;
-        exit(0);
-        break;
-
-    }
-
-    }
-    return 0;*/
 }
 
 
@@ -269,13 +87,19 @@ void Register(string user_name, int user_id, string user_pass, string user_gende
 void Login(string, string) {
 
 }
-void Password_validation(string) {
-
-}
-void ID_validation(string) {
-
+bool Password_ID_validation(int id , string password)
+{
+    for (int i = 0; i < users_vec.size(); i++)
+    {
+        if (users_vec[i].id == id && users_vec[i].password == password)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 void DisplayMyRecord() {
+    cout << "Name : " << users.at(Myid).fullName << "\nGender : " << users.at(Myid).gender << "\nAge : " << users.at(Myid).age << "\nGovernorate : " << users.at(Myid).governorate << "\nNum of doses : " << users.at(Myid).num_of_dose << endl;
 
 }
 void edit_user_data(string choose) {
@@ -285,7 +109,13 @@ void edit_user_data(string choose) {
     }
     else if (choose == "id")
     {
-        cin >> users.at(Myid).id;
+        int id;
+        do
+        {
+            cin >> id;
+        } while (ID_validation(id) == false);
+        
+        users.at(Myid).id=id;
     }
     else if (choose == "gender")
     {
@@ -311,9 +141,6 @@ void edit_user_data(string choose) {
 void Delete_MyACC() 
 {
     users.erase(Myid);
-}
-void vaccinationState_stat() {
-
 }
 void accepting_vaccinated(int number_of_user)
 {
@@ -453,6 +280,112 @@ void save()
 
     
 }
+bool ID_validation(int id)
+{
+    if (usersID.find(id) != usersID.end())
+    {
+        return true;
+    }
+    else
+        return false;
+}
+void mainMenu()
+{
+    
+    int choice;
+    int option;
+    int your_choice;
+    string x;
+    int id;
+    string password;
+    string username;
+    cout << endl;
+    cout << "               ***********************************************           " << endl;
+    cout << "                    WELCOME TO VACCINE TRACKING SYSTEM                    " << endl;
+    cout << "               ***********************************************           " << endl;
+    cout << endl;
+    cout << endl;
+    cout << "\t1. Log in" << endl;
+    cout << "\t2. Register as a new user" << endl;
+    cout << "\t3. Exit Program" << endl;
+    cout << "\tPlease enter number of your choice: ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        bool flag = false;
+        cout << "Enter ID : ";
+        cin >> id;
+        cout << "Enter password : ";
+        cin >> password;
+        do
+        {
+
+            
+            if (id == 0 && password == "admin")
+            {
+                system("cls");
+                adminMenu();
+
+                break;
+            }
+            
+            if (flag)
+            {
+                int c;
+                cout << "Do u want to Renter Id and password or go back" << endl;
+                cout << "1 -> Renter" << endl;
+                cout << "2 -> Go back" << endl;
+                cout << "Your choice : ";
+                cin >> c;
+                switch (c)
+                {
+                case 1:
+                    cout << "Enter ID : ";
+                    cin >> id;
+                    cout << "Enter password : ";
+                    cin >> password;
+                    break;
+                case 2:
+                    system("cls");
+                    mainMenu();
+                    break;
+                default:
+                    flag = true;
+                    continue;
+
+                }
+
+
+            }
+            if (Password_ID_validation(id, password) == true)
+            {
+                break;
+            }
+            else
+                flag = true;
+
+        } while (Password_ID_validation(id, password) == false);
+
+        Myid = id;
+        system("cls");
+        break;
+    }
+
+    case 2:
+    {
+
+       
+        break;
+
+    }
+
+
+    
+
+    }
+}
 
 
 //----------------------  admin -----------------------------
@@ -461,28 +394,44 @@ void display_perc_of_gender(char kind)
 {
     float num_of_boys = 0;
     float num_of_girls = 0;
-    for (int i = 0; i < users_vec.size(); i++)
+    if (users_vec.size() != 1)
     {
-        if (users_vec[i].gender == "male")
+        for (int i = 0; i < users_vec.size(); i++)
         {
-             num_of_boys++;
-           
+            if (users_vec[i].gender == "male")
+            {
+                num_of_boys++;
+
+            }
+            else
+            {
+                num_of_girls++;
+
+            }
+        }
+    }
+   
+    bool done = true;
+    do
+    {
+        if (kind == 'm' || kind == 'M') {
+
+            cout << (num_of_boys / TOTALUSERS) * 100 << "%\n";
+            cout << "f" << endl;
+            done = false;
+            break;
+        }
+        else if (kind == 'f' || kind == 'F') {
+            cout << (num_of_girls / TOTALUSERS) * 100 << "%\n";
+            done = false;
+            break;
         }
         else
-        {
-            num_of_girls++;
-
-        }
-    }
-    if (kind == 'm' || kind == 'M') {
-        
-        cout << (num_of_boys / TOTALUSERS) * 100<< "%\n";
-    }
-    else if (kind == 'f' || kind == 'F') {
-        cout << (num_of_girls / TOTALUSERS) * 100 << "%\n";
-    }
-    else
-        cout << "Wrong Choice please choose eithe m or f\n";
+            cout << "Wrong Choice please choose eithe m or f\n";
+        cout << "M : Male      F : Femals" << endl;
+        cout << "Choose a char : ";
+        cin >> kind;
+    } while (done);
 
 }
 void display_perc_of_doses(int dose_num)
@@ -507,18 +456,32 @@ void display_perc_of_doses(int dose_num)
 
         }
     }
-    if (dose_num == 0) {
-        cout << (num_of_0 / num_of_doses) * 100 << "%\n";
-    }
-    else if (dose_num == 1) {
-        cout << (num_of_1 / num_of_doses) * 100 << "%\n";
-    }
-    else if (dose_num == 2) {
-        cout << (num_of_2 / num_of_doses) * 100 << "%\n";
-    }
-    else {
-        cout << "Wrong information\n";
-    }
+    bool done = true;
+    do
+    {
+        if (dose_num == 0) {
+            cout << (num_of_0 / num_of_doses) * 100 << "%\n";
+            done = false;
+            break;
+        }
+        else if (dose_num == 1) {
+            cout << (num_of_1 / num_of_doses) * 100 << "%\n";
+            done = false;
+            break;
+
+        }
+        else if (dose_num == 2) {
+            cout << (num_of_2 / num_of_doses) * 100 << "%\n";
+            done = false;
+            break;
+        }
+        else {
+            cout << "Wrong information\n";
+            cout << "Number of doses : ";
+            cin >> dose_num;
+        }
+    } while (done);
+    
 }
 void display_record(int id)
 {
@@ -539,22 +502,116 @@ void display_record(int id)
 void display_records_filterd(string num_doses, int age)
 {
     bool flag = true;
-    for (int i = 0; i < users_vec.size(); i++)
+    do
     {
-        if (users_vec[i].age == age && users_vec[i].num_of_dose==num_doses) {
-            cout << "Name : " << users_vec[i].fullName << "\nGender : " << users_vec[i].gender << "\nAge : " << users_vec[i].age << "\nGovernorate : " << users_vec[i].governorate << endl;
-            cout << "--------------------------------------------\n";
-            flag = false;
+        for (int i = 0; i < users_vec.size(); i++)
+        {
+            if (users_vec[i].age == age && users_vec[i].num_of_dose == num_doses) {
+                cout << "Name : " << users_vec[i].fullName << "\nGender : " << users_vec[i].gender << "\nAge : " << users_vec[i].age << "\nGovernorate : " << users_vec[i].governorate << endl;
+                cout << "--------------------------------------------\n";
+                flag = false;
+            }
         }
-    }
-    if (flag)
-    {
-        cout << "user not found" << endl;
-    }
+        if (flag)
+        {
+            cout << "user not found" << endl;
+            cout << "Number of doses : ";
+            cin >> num_doses;
+            cout << "AGE : ";
+            cin >> age;
+        }
+    } while (flag);
+    
 }
 void delete_record(int id)
 {
     users.erase(id);
-}
+    usersID.erase(usersID.find(id));
 
+}
+void adminMenu()
+{
+    bool done = true;
+    do
+    {
+        int c;
+        cout << "               ***********************************************           " << endl;
+        cout << "                            WELCOME TO Admin page                        " << endl;
+        cout << "               ***********************************************           " << endl;
+        cout << "1 -> Display percentage of gender" << endl;
+        cout << "2 -> Display percentage of doses" << endl;
+        cout << "3 -> Display records" << endl;
+        cout << "4 -> Display a record" << endl;
+        cout << "5 -> Display records filterd" << endl;
+        cout << "6 -> Delete a record" << endl;
+        cout << "7 -> Exit" << endl;
+        cout << "Choose a number : ";
+        cin >> c;
+        switch (c)
+        {
+        case 1:
+        {
+            char c2;
+            cout << "M : Male      F : Femals" << endl;
+            cout << "Choose a char : ";
+            cin >> c2;
+            display_perc_of_gender(c2);
+            break;
+        }
+
+
+        case 2:
+        {
+            int c2;
+            cout << "Number of doses : ";
+            cin >> c2;
+            display_perc_of_doses(c2);
+            break;
+        }
+        case 3:
+        case 4:
+        {
+            int c2;
+            cout << "Enter ID : ";
+            do
+            {
+                cin >> c2;
+            } while (ID_validation(c2) == false);
+            display_record(c2);
+            break;
+        }
+
+        case 5:
+        {
+            string c2;
+            int c3;
+            cout << "Number of doses : ";
+            cin >> c2;
+            cout << "AGE : ";
+            cin >> c3;
+            display_records_filterd(c2, c3);
+            break;
+        }
+        case 6:
+        {
+            int c2;
+            cout << "Enter ID : ";
+            do
+            {
+
+                cin >> c2;
+            } while (ID_validation(c2) == false);
+            delete_record(c2);
+            break;
+        }
+
+
+        default:
+            done = false;
+            break;
+        }
+    } while (done);
+    system("cls");
+    mainMenu();
+}
 //-----------------------------------------------------------
